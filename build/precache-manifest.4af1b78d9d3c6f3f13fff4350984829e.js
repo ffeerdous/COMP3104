838ef7d26ca639d7f41577934a3188510eb65b65
ffeerdous <ferdousazizi9@gmail.com>
2022-10-07 17:02:08 -0400

    Github action script update

---
+++ build/precache-manifest.4af1b78d9d3c6f3f13fff4350984829e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eca35b3b5b7a369d91a6",
+    "url": "/ffeerdous/COMP3104/static/css/main.6639af86.chunk.css"
+  },
+  {
+    "revision": "eca35b3b5b7a369d91a6",
+    "url": "/ffeerdous/COMP3104/static/js/main.eca35b3b.chunk.js"
+  },
+  {
+    "revision": "5942be486d11ed696b54",
+    "url": "/ffeerdous/COMP3104/static/js/runtime~main.5942be48.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/ffeerdous/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "9b1e03cb3f2ebfc5d92fcf571e821339",
+    "url": "/ffeerdous/COMP3104/index.html"
+  }
+];
